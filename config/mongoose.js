8fd9d7d2dce42925ff2b0f7ea3f1af1ee2ba324d
harish kumar <harishpathania150@gmail.com>
2023-12-22 12:40:19 +0530

    updated the code

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,20 +1,19 @@
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', true);
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 
-const env = require('./environment');
-const { FALSE } = require('node-sass');
-// mongoose.connect(`mongodb://127.0.0.1:27017/${env.db}`);
-mongoose.connect('mongodb+srv://harish:harish123@cluster0.0qpeyqk.mongodb.net/codeial_development');
-
+const env = require("./environment");
+mongoose.connect(
+  `mongodb+srv://harish:harish123@cluster0.0qpeyqk.mongodb.net/${env.db}`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+// mongoose.connect('mongodb+srv://harish:harish123@cluster0.0qpeyqk.mongodb.net/codeial_development',{ useNewUrlParser: true });
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
 
-
-db.once('open', function(){
-    console.log('Connected to Database :: MongoDB');
+db.once("open", function () {
+  console.log("Connected to Database :: MongoDB");
 });
-
 
 module.exports = db;
