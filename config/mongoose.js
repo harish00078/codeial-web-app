72370e5efeee39a543c6fbd3b0181957209207d7
harish kumar <harishpathania150@gmail.com>
2023-12-22 13:36:49 +0530

    resolving the errors

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const env = require('./environment');
-mongoose.connect(`mongodb://localhost/${env.db}`);
+// mongoose.connect(`mongodb://localhost/${env.db}`);
+mongoose.connect(`mongodb+srv://harish:harish123@cluster0.0qpeyqk.mongodb.net/${env.db}`,{
+     useNewUrlParser: true ,useUnifiedTopology:true
+});
 
 const db = mongoose.connection;
 
