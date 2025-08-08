7d7d59ccdcb903a03b48fe52fdf214acc1483e4f
harish kumar <harishpathania150@gmail.com>
2025-08-08 21:27:22 +0530

    update css and why we are not able to access it in development env

--- config/view-helpers.js
+++ config/view-helpers.js
@@ -6,7 +6,14 @@ const path = require('path');
 module.exports = (app) => {
     app.locals.assetPath = function(filePath){
         if (env.name == 'development'){
-            return filePath;
+            // Add cache-busting based on file mtime in development
+            try {
+                const fullPath = path.join(__dirname, '..', env.asset_path, filePath);
+                const mtime = fs.statSync(fullPath).mtime.getTime();
+                return `/${filePath}?v=${mtime}`;
+            } catch (e) {
+                return `/${filePath}?v=${Date.now()}`;
+            }
         }
 
         return '/' + JSON.parse(fs.readFileSync(path.join(__dirname, '../public/assets/rev-manifest.json')))[filePath];
